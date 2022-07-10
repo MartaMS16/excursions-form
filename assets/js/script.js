0504a10fd587b266cb2a005f1b65b90f486ba147
Marta Stelmach <marta.ms1961@gmail.com>
2022-07-10 17:16:55 +0200

    Render excursions

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,20 +12,44 @@ function init() {
 
 function previewFileContent() {
     const reader = new FileReader();
-    loadFile(reader);
+    const container = document.querySelector('.excursions');
+    loadFile(reader, container);
 };
 
-function loadFile(reader) {
-    const container = document.querySelector('.excursions');
+function loadFile(reader, container) {
     const [file] = document.querySelector('input[type=file]').files;
 
     reader.addEventListener(
-        "load", function () {
-            container.innerText = reader.result;
+        "load",
+        function () {
+            const readerContent = Array.from(reader.result.split(/[\r\n]+/gm));
+            const excursions = readerContent.map(function (el) {
+                return el.split(/"?,?"/).slice(1, 6);
+            });
+            excursions.forEach(function (excursion) {
+                renderExcursion(container, excursion);
+            });
         }
     );
 
     if (file) {
-        reader.readAsText(file);
+        reader.readAsText(file, 'UTF-8');
     };
+};
+
+function renderExcursion(container, excursion) {
+    const prototype = document.querySelector('.excursions__item--prototype');
+    const li = prototype.cloneNode(true);
+    const excursionTitle = li.querySelector('.excursions__title');
+    const excursionDescription = li.querySelector('.excursions__description');
+    const excursionsPrice = li.querySelector('.excursions__form');
+    const priceForAdult = excursionsPrice.querySelectorAll('span')[0];
+    const priceForChild = excursionsPrice.querySelectorAll('span')[1];
+    li.classList.remove('excursions__item--prototype');
+    excursionTitle.innerText = excursion[1];
+    excursionDescription.innerText = excursion[2];
+    priceForAdult.innerText = excursion[3] + ' ';
+    priceForChild.innerText = excursion[4] + ' ';
+
+    container.appendChild(li);
 };
